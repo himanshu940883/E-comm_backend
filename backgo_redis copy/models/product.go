9ce454bd66ef4ec04e41107278237f9fa4df31b3
himanshu940883 <himanshu.g@appsforbharat.com>
2025-05-30 17:51:47 +0530

    Delete backgo_redis copy directory

--- backgo_redis copy/models/product.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type Product struct {
-	ID    int    `db:"id" json:"id"`
-	Name  string `db:"name" json:"name"`
-	Price int    `db:"price" json:"price"`
-	Image string `db:"image" json:"image"`
-}
--- backgo_redis copy/models/user.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type User struct {
-	ID       int    `db:"id" json:"id"`
-	UserID   string `db:"user_id" json:"user_id"`
-	Password string `db:"password" json:"password"`
-	Role     string `db:"role" json:"role"`
-}
